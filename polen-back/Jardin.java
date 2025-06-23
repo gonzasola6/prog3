7ef7b03a06582fbe66102bbc7584500d468a9f90
gonzasola6 <146140655+gonzasola6@users.noreply.github.com>
2025-06-23 16:19:23 -0300

    Add files via upload

---
+++ polen-back/Jardin.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class Jardin {
+    List<Flor> flores;
+
+    public List<Flor> floresCercanas(Coordenada pos, int disMaxima) { // flores cercanas a esta flor con este maximo de
+                                                                      // distancai
+        return null;
+    }
+}
